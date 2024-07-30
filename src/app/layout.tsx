532ecc937171445f82a5fea5a09a81a6c3922a5f
isabla <blais.isael.entr@gmail.com>
2024-07-29 20:43:37 -0400

    chore(shadcn-ui): Init shadcn-ui with tailwind (layout.tsx, tailwind.config.ts, globals.css)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import "~/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
 
-import { GeistSans } from "geist/font/sans";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 import { type Metadata } from "next";
+import { cn } from "~/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -13,8 +18,15 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
