e1e9a9fdc9e61939d2b124fd53ae0293a5575655
isabla <blais.isael.entr@gmail.com>
2024-07-29 22:42:38 -0400

    feat: Fetch server-side users list from the db & console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { CardWithForm } from "~/components/ui/card-with-form";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const users = await db.query.user.findMany();
+
+  console.log("users: ", users);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
